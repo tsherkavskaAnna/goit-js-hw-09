aec46e64bd0d2a5a65bc5f00b70648ec856a43b1
Anna Tsherkavska <katotoka1985@gmail.com>
2022-04-02 17:10:53 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  const btnStart = document.querySelector(`[data-start]`);
+  console.log(btnStart);
+  const btnStop = document.querySelector(`[data-stop]`);
+  console.log(btnStop);
+  const bodyColor = document.querySelector(`body`);
+  let timerId = null;
+
+  btnStart.addEventListener(`click`, () => {
+      timerId = setInterval(() => {
+          bodyColor.style.backgroundColor = getRandomHexColor()
+      }, 1000);
+      btnStart.disabled = true;
+  });
+
+  btnStop.addEventListener(`click`, () => {
+      clearInterval(timerId);
+      btnStart.disabled = false;
+  })

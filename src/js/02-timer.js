4e8bb0b3bad4d9d024501e57980ce7bee5e653ca
Anna Tsherkavska <katotoka1985@gmail.com>
2022-04-11 11:56:50 +0200

    Update task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,11 +2,12 @@
 import flatpickr from "flatpickr";
 // Дополнительный импорт стилей
 import "flatpickr/dist/flatpickr.min.css";
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from "notiflix";
+
 
 
   const refs = {
-      input: document.querySelector(`input`),
+      inputTime: document.querySelector(`input`),
       btnStart: document.querySelector(`button[data-start]`),
       days: document.querySelector(`[data-days]`),
       hours: document.querySelector(`[data-hours]`),
@@ -14,20 +15,70 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
       seconds: document.querySelector(`[data-seconds]`), 
   };
 
+refs.btnStart.disabled = true;
+
+let selectedDate = null;
+let timerId = null;
+
   const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
+      selectedDate = selectedDates[0].getTime();
+      if(selectedDate < new Date()) {
+        Notiflix.Notify.failure(`Please choose a date in the future`);
+        return;
+      }
+      refs.btnStart.disabled = false;
     },
   };
 
-  const timer = new Date;
-  console.log(timer);
-  const date = Date.now();
-  console.log(date)
+flatpickr(`#datetime-picker`, options);
 
-  refs.btnStart.addEventListener(`click`, flatpickr);
+refs.btnStart.addEventListener(`click`, startTimer);
+
+function startTimer() {
+  setInterval(() => {
+    const deltaDate = selectedDate - Date.now();
+    if(deltaDate <= 0) {
+      return;
+    }
+ const {days, hours, minutes, seconds} = convertMs(deltaDate);
+
+   refs.days.textContent = days;
+   refs.hours.textContent = hours;
+   refs.minutes.textContent = minutes;
+   refs.seconds.textContent = seconds;
+
+  }, 1000)
+ }
+  
+ function addLeadingZero(value) {
+   return String(value).padStart(2, `0`);
+ }
+
+ function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 

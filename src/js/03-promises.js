2b131b950b224d7413e4616224f5591187963845
Anna Tsherkavska <katotoka1985@gmail.com>
2022-04-11 18:50:00 +0200

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix, { Notify } from "notiflix";
+
+const refs = {
+  form: document.querySelector(`form`),
+  delay: document.querySelector(`[name = delay]`),
+  step: document.querySelector(`[name = step]`),
+  amount: document.querySelector(`[name = amount]`),
+};
+
+refs.form.addEventListener(`submit`, handlerSubmit);
+
+function handlerSubmit(event) {
+  event.preventDefault();
+
+  const { delay, step, amount } = event.currentTarget;
+  let firstDelay = Number(refs.delay.value);
+  let amountCount = Number(refs.amount.value);
+  let stepDelay = Number(refs.step.value);
+
+  for (let i = 1; i < amountCount; i++) {
+    cretePromise(i, firstDelay)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+firstDelay += stepDelay;
+  };
   }
-}
+
+  function cretePromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({position, delay});
+        }
+        reject({position, delay});
+      }, delay)
+    });
+  }
